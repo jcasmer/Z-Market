d1ab27a314f85d77673a91e85ac1df9e1692929f
Juan Esteban Castaño Mera <juan.castano@emtelco.com.co>
2018-05-30 17:55:01 -0500

    se agrega texto que muestra

--- Z-Market/Z-Market/Models/DocumentType.cs
+++ Z-Market/Z-Market/Models/DocumentType.cs
@@ -9,7 +9,9 @@ namespace Z_Market.Models
     public class DocumentType
     {
         [Key]
+        [Display(Name = "Tipo Documento")]
         public int DocumentTypeID { get; set; }
+        [Display(Name = "Tipo Documento")]
         public string Description { get; set; }
 
         public virtual ICollection<Employee> Employees { get; set; }
--- Z-Market/Z-Market/Models/Employee.cs
+++ Z-Market/Z-Market/Models/Employee.cs
@@ -10,14 +10,33 @@ namespace Z_Market.Models
     {
         [Key]
         public int EmployeeID { get; set; }
+
+        [Display(Name="Nombre")]
+        [Required(ErrorMessage="Debe ingresar {0}")]
         public string FirstName { get; set; }
+
+        [Display(Name = "Apellido")]
+        [Required(ErrorMessage = "Debe ingresar {0}")]
         public string LastName { get; set; }
+
+        [Display(Name = "Salario")]
+        [Required(ErrorMessage = "Debe ingresar {0}")]
         public decimal Salary{ get; set; }
+
+        [Display(Name = "Porcentaje Bono")]
         public float BonusPercent { get; set; }
+
+        [Display(Name = "Día de nacimiento")]
+        [Required(ErrorMessage = "Debe ingresar {0}")]
         public DateTime DateOfBirth { get; set; }
+        [Display(Name = "Hora inicio")]
         public DateTime StartTime { get; set; }
+        [Display(Name = "Correo electrónico")]
         public string Email { get; set; }
+        [Display(Name = "Url")]
         public string URL { get; set; }
+
+        [Required(ErrorMessage = "Debe ingresar {0}")]
         public int DocumentTypeID { get; set; }
 
         public virtual DocumentType DocumentType { get; set; } // empleado relacion con tipo de documento
